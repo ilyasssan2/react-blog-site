e9d1775d78b191cfffe023f8c425500b5e317a75
ilyasssan <boudakailyass@gmail.com>
2020-12-28 15:01:06 +0100

    WORKING ON SERVICES PAGE

--- src/pages/Services.js
+++ src/pages/Services.js
@@ -6,6 +6,40 @@ function Services() {
   return (
     <div>
       <PageTop page="Services" />
+      <div className="services ">
+        <div className="container">
+          <div className="row">
+            <div className="col-lg-4  service active ">
+              <div>
+                <img src="/assets/images/services/s2.svg" alt="" />
+              </div>
+              <div>
+                <h5>Hello there</h5>
+                <p>Lorem ipsum dolor sit, amet consectetur</p>
+              </div>
+            </div>
+
+            <div className="col-lg-4   service  ">
+              <div>
+                <img src="/assets/images/services/s2.svg" alt="" />
+              </div>
+              <div>
+                <h5>Hello there</h5>
+                <p>Lorem ipsum dolor sit, amet consectetur</p>
+              </div>
+            </div>
+            <div className="col-lg-4  service ">
+              <div>
+                <img src="/assets/images/services/s3.svg" alt="" />
+              </div>
+              <div>
+                <h5>Hello there</h5>
+                <p>Lorem ipsum dolor sit, amet consectetur</p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
